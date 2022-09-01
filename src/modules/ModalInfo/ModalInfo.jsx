f8883885fdb2a88ba5799d3bdd5133c693572369
Mikhail Potapov <86321783+mishapotap@users.noreply.github.com>
2022-09-01 18:32:39 +0300

    Правки по контенту от 01.09.2022

--- src/modules/ModalInfo/ModalInfo.jsx
+++ src/modules/ModalInfo/ModalInfo.jsx
@@ -5,14 +5,14 @@ const ModalInfo = ({ data }) => {
 	return (
         <div className={styles.wrapper}>
             <div className={styles.column}>
-                <div className={styles.text}>Условное обозначение:</div>
-                <div className={styles.text}>Старт:</div>
-                <div className={styles.text}>Стыковка:</div>
+                {data.symbol && <div className={styles.text}>Условное обозначение:</div>}
+                {data.start && <div className={styles.text}>Старт:</div>}
+                {data.finish && <div className={styles.text}>Стыковка:</div>}
             </div>
             <div className={styles.column}>
-                <div className={styles.textBlue}>{data.symbol || "Нет информации"}</div>
-                <div className={styles.textBlue}>{data.start || "Нет информации"}</div>
-                <div className={styles.textBlue}>{data.finish || "Нет информации"}</div>
+                {data.symbol && <div className={styles.textBlue}>{data.symbol}</div>}
+                {data.start && <div className={styles.textBlue}>{data.start}</div>}
+                {data.finish && <div className={styles.textBlue}>{data.finish}</div>}
             </div>
         </div>
 	);
